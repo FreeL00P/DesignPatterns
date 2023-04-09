db1bead890ff62a333edb4ad6b06c4f5ff12e083
FJ <qq1084472249@gmail.com>
2023-04-09 15:26:29 +0800

    适配器模式:接口适配器

---
+++ src/main/java/com/freeloop/adapter/interfaceadapter/AbsAdapter.java
@@ -0,0 +1,30 @@
+package com.freeloop.adapter.interfaceadapter;
+
+/**
+ * AbsAdapter
+ *
+ * @author fj
+ * @since 2023/4/9 14:38
+ */
+//将Interface4 的方法全部实现
+public abstract class AbsAdapter implements Interface4{
+    @Override
+    public void m1() {
+
+    }
+
+    @Override
+    public void m2() {
+
+    }
+
+    @Override
+    public void m3() {
+
+    }
+
+    @Override
+    public void m4() {
+
+    }
+}
---
+++ src/main/java/com/freeloop/adapter/interfaceadapter/Interface4.java
@@ -0,0 +1,14 @@
+package com.freeloop.adapter.interfaceadapter;
+
+/**
+ * Interface4
+ *
+ * @author fj
+ * @since 2023/4/9 14:38
+ */
+public interface Interface4 {
+    void m1();
+    void m2();
+    void m3();
+    void m4();
+}
